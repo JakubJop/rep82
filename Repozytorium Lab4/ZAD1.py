617a073ad527188b5ef261dfec7fae243efbb5e0
w67238 <kuba_04042@interia.pl>
2022-11-03 18:47:03 +0100

    zad1

---
+++ Repozytorium Lab4/ZAD1.py
@@ -0,0 +1,13 @@
+""""Utwórz listę lista_zakupow zawierającą nazwy co najmniej czterech produktów spożywczych.
+Następnie:
+• wyświetl pierwszy i drugi element z listy
+• zmień trzeci element listy na „mleko”
+• wyświetl przedostatni i ostatni element listy"""
+
+
+lista_zakupów = {"woda","ser","chleb","sok"}
+print(lista_zakupów[0;2])
+print(lista zakupów)
+
+
+
